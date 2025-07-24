2da75ed576a7914736eefb8e5f62912b1fca513d
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-07-24 17:30:34 +0530

    Completed

---
+++ script.js
@@ -0,0 +1,40 @@
+Shery.mouseFollower(); //creates mouse follower
+
+Shery.makeMagnet(".magnet"); //creats magnet effect
+
+Shery.hoverWithMediaCircle(".hvr", {videos:["videos/0.mp4", "videos/2.mp4", "videos/3.mp4"] });//play video when hover
+// console.log("THREE is:", THREE);
+
+gsap.to(".fleftelm",{                    //gsap controlls animation
+    scrollTrigger:{              //scrollTrigger stops screen
+        trigger: "#fimages",
+        pin: true,
+        start: "top top",
+        end:"bottom bottom",
+        endTrigger : ".last",
+        scrub: 1
+    },
+    y: "-300%",
+    ease: Power1
+})
+
+let sections = document.querySelectorAll(".fleftelm")
+Shery.imageEffect(".images", {
+  style: 5,
+  config : {onMouse : {value : 1}},
+  slideStyle: (setScroll) => {
+    sections.forEach(function(section, index){
+        ScrollTrigger.create({
+            trigger: section,
+            start : "top top",
+            scrub : 1,
+            onUpdate : function(prog){
+                // console.dir(prog);
+                setScroll(prog.progress+index)
+            }
+        })
+    })
+  },
+});
+
+Shery.makeMagnet(".btndiv button")
